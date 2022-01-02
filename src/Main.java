546d226fe1ecd56f09bf4587c0fcdb1349c894a8
Vadim Uzhakin <vadimuzhakin@gmail.com>
2022-01-02 23:18:04 +0700

    modified java

--- src/Main.java
+++ src/Main.java
@@ -3,8 +3,14 @@ public class Main {
         CreditPaymentService service = new CreditPaymentService();
         double total = 1_000_000.00;
         int creditMonths = 12;
+        int creditMonths2 = 24;
+        int creditMonths3 = 36;
         double pay = service.calculate(total, creditMonths);
+        double pay2 = service.calculate(total, creditMonths2);
+        double pay3 = service.calculate(total, creditMonths3);
         System.out.println("Ваш платеж состовляет: " + pay + " рубль(ей)");
+        System.out.println("Ваш платеж состовляет: " + pay2 + " рубль(ей)");
+        System.out.println("Ваш платеж состовляет: " + pay3 + " рубль(ей)");
     }
 }
 
