4ee32f6c4e73fe93b9fb47ffd0ecd9854a78469a
Vadim Uzhakin <vadimuzhakin@gmail.com>
2022-01-10 13:43:37 +0700

    modified Java

--- src/CreditPaymentService.java
+++ src/CreditPaymentService.java
@@ -1,15 +1,6 @@
 public class CreditPaymentService {
     public double calculate(double total, int creditMonths) {
-        double monthRate = 9.99 / creditMonths / 100;
-        if (creditMonths == 12) {
-            monthRate = 9.99 / creditMonths / 100;
-        }
-        if (creditMonths == 24) {
-            monthRate = (9.99 / creditMonths / 100) * 2;
-        }
-        if (creditMonths == 36) {
-            monthRate = (9.99 / creditMonths / 100) * 3;
-        }
+        double monthRate = 9.99 / (12 * 100);
         double amount = 1.00 + monthRate;
         double payMonth = total * (monthRate + (monthRate / ((Math.pow(amount, creditMonths)) - 1)));
         return payMonth;
